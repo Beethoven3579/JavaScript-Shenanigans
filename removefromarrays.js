9a0966449da0d0098c3b4674dc1755fcc7ce195c
Beethoven3579 <75546304+Beethoven3579@users.noreply.github.com>
2021-01-18 22:06:01 -0500

    Add files via upload
    More algorithm scripting here! Heres a few more from freeCodeCamp

---
+++ removefromarrays.js
@@ -0,0 +1,31 @@
+/*Intermediate Algorithm Scripting: Seek and Destroy
+
+You will be provided with an initial array (the first argument in 
+the destroyer function), followed by one or more arguments. Remove 
+all elements from the initial array that are of the same value as these 
+arguments.
+
+Note
+You have to use the arguments object. */
+
+// Solution 1  
+
+function destroyer(arr) {
+
+    const args = Array.from(arguments);
+    args.splice(0,1);
+    const targets = args;
+    const result = [];
+    
+    for (let num of arr) {
+      if (targets.indexOf(num) === -1) {
+        result.push(num);
+      }
+    }
+    
+      return result;
+    }
+    
+    console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
+
+    // Solution 2
